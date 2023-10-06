437b048dbc0f7a4cd960c5527b1cf3805e987c4e
Rebepekka <rebekka23001@student.hamk.fi>
2023-10-06 19:45:54 +0300

    Aloitus: scannerin lisäys ja kysymys käyttäjälle.

--- src/ArvausPeli.java
+++ src/ArvausPeli.java
@@ -1,9 +1,22 @@
-
+import java.util.Scanner;
 public class ArvausPeli {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner keyboardInput = new Scanner(System.in);
+		
+		String secretWord ="Kukka";
+		String guess = "";
+		
+		while(!guess.equals(secretWord))
+		{
+			System.out.println("Arvaa nimi:");
+			guess = keyboardInput.nextLine();
+		}
+				
+		
+		
+		
 	}
 
 }
